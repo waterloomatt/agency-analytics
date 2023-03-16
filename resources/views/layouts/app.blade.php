65bd4f854ddc40ebba42f3429210a34c634b949e
Matt Skelton <waterloomatt@gmail.com>
2023-03-16 02:07:53 -0400

    Apply some basic styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,10 @@
 <html lang="">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <title>AgencyAnalytics - @yield('title')</title>
 </head>
 <body>
