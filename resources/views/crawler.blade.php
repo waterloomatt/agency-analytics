38d331e6da562bd1e7f42c9c6e7a6174e386937e
Matt Skelton <waterloomatt@gmail.com>
2023-03-16 05:14:44 -0400

    More styling

--- resources/views/crawler.blade.php
+++ resources/views/crawler.blade.php
@@ -3,31 +3,188 @@
 @section('title', 'Crawler')
 
 @section('content')
-    <h1>AgencyAnalytics - Crawler</h1>
-    @error('url')
-    <div class="alert alert-danger">{{ $message }}</div>
-    @enderror
+    <div>
+        <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="mt-5 md:col-span-2 md:mt-0">
+                <h1 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                    AgencyAnalytics - Web Crawler
+                </h1>
 
-    <form action=""
-          method="POST">
-        @csrf
+                <div class="mt-10">
+                    <form method="POST">
+                        @csrf
+                        <div class="shadow sm:overflow-hidden sm:rounded-md">
 
-        <label>
-            URL
-            <input type="text"
-                   name="url"
-                   placeholder="https://agencyanalytics.com">
-        </label>
+                            <h2 class="ml-3 my-3 text-lg font-bold leading-4 text-gray-900 sm:truncate sm:text-xl sm:tracking-tight">
+                                Start Crawl
+                            </h2>
 
-        <input type="submit"
-               name="submit">
-    </form>
+                            <div class="space-y-6 bg-white px-4 py-5 sm:p-6">
+                                <div class="grid grid-cols-3 gap-6">
+                                    <div class="col-span-3 sm:col-span-2">
+                                        <label for="url"
+                                               class="block text-sm font-medium leading-6 text-gray-900">Website</label>
+                                        <div class="mt-2 flex rounded-md shadow-sm">
+                                            <span class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 px-3 text-gray-500 sm:text-sm">https://</span>
+                                            <input type="text"
+                                                   name="url"
+                                                   id="url"
+                                                   class="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                   placeholder="www.example.com">
+                                        </div>
+
+                                        @error('url')
+                                        <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+                                            {{ $message }}
+                                        </span>
+                                        @enderror
+                                    </div>
 
 
+                                </div>
 
-    @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
+                                <div>
+                                    <label for="pages"
+                                           class="block text-sm font-medium leading-6 text-gray-900">
+                                        Pages to crawl (min 1, max 10)
+                                    </label>
+                                    <div class="mt-2">
+                                        <input type="number"
+                                               value="5"
+                                               min="1"
+                                               max="10"
+                                               name="pages"
+                                               id="pages"
+                                               class="block w-13 flex-1 rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                    </div>
+
+                                    @error('pages')
+                                    <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">
+			{{ $message }}
+		</span>
+                                    @enderror
+
+                                </div>
+                            </div>
+                            <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                                <button type="submit"
+                                        class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                                    Start Crawl
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="shadow sm:overflow-hidden sm:rounded-md">
+                    <h2 class="ml-3 my-3 text-lg font-bold leading-4 text-gray-900 sm:truncate sm:text-xl sm:tracking-tight">
+                        Crawl History
+                    </h2>
+
+                    <table class="min-w-full leading-normal">
+                        <thead>
+                        <tr>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Created
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Pages
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Unique Images
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Unique Internal Links
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Unique External Links
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Avg. Page Load
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Avg. Word Count
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Avg. Title Length
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                                Status
+                            </th>
+                            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100">
+                                Details
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach($crawls as $crawl)
+                            <tr>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <div class="flex">
+                                        <div class="ml-3">
+                                            <p class="text-gray-900 whitespace-no-wrap">
+                                                {{ $crawl->created_at->diffForHumans() }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{{ $crawl->pages }}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{{ $crawl->unique_images }}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{{ $crawl->unique_internal_links }}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{{ $crawl->unique_external_links }}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{{ $crawl->avg_page_load }}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{{ $crawl->avg_word_count }}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">{{ $crawl->avg_title_length }}</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                      <span class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                      <span class="relative">
+                                          {{ $crawl->status }}
+                                      </span>
+                                    </span>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
+                                    @include('partials.crawl_details_modal', ['crawl' => $crawl, 'modal_id' => 'modal_'.$crawl->id])
+                                    <button type="button"
+                                            class="inline-block text-gray-500 hover:text-gray-700"
+                                            onclick="toggleModal('modal_{{ $crawl->id }}')">
+                                        <svg class="inline-block h-6 w-6 fill-current"
+                                             viewBox="0 0 24 24">
+                                            <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z"/>
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-    @endif
+    </div>
+
+    <script type="text/javascript">
+        function toggleModal(modalID){
+            document.getElementById(modalID).classList.toggle("hidden");
+            document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+            document.getElementById(modalID).classList.toggle("flex");
+            document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+        }
+    </script>
+
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
