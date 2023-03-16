d1308975df4c1b088829848d6af1b564e5f9fa5a
Matt Skelton <waterloomatt@gmail.com>
2023-03-15 20:32:03 -0400

    Add relationships

--- app/Models/Crawl.php
+++ app/Models/Crawl.php
@@ -5,12 +5,19 @@ namespace App\Models;
 use App\Enums\CrawlStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Crawl extends Model
 {
     use HasFactory;
 
+    protected $guarded  = [];
     protected $casts = [
         'status' => CrawlStatus::class,
     ];
+
+    public function details(): HasMany
+    {
+        return $this->hasMany(CrawlDetail::class);
+    }
 }
--- app/Models/CrawlDetail.php
+++ app/Models/CrawlDetail.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CrawlDetail extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Crawl::class);
+    }
 }
