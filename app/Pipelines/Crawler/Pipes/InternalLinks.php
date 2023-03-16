5f76049492e906948bbd2e38f12f929ff1770d07
Matt Skelton <waterloomatt@gmail.com>
2023-03-15 22:38:46 -0400

    Minor cleanup

--- app/Pipelines/Crawler/Pipes/InternalLinks.php
+++ app/Pipelines/Crawler/Pipes/InternalLinks.php
@@ -9,19 +9,26 @@ use Illuminate\Support\Str;
 
 class InternalLinks
 {
+    public const CSS_SELECTOR = 'a[href*=agencyanalytics.com], a[href^=/], a[href^=#]';
+
     public function handle(CrawlResult $result, Closure $next)
     {
-        $internalLinks = $result->document->find('a[href*=agencyanalytics.com], a[href^=/]');
+        $internalLinks = $result->document->find(self::CSS_SELECTOR);
 
-        foreach ($internalLinks as $link) {
-            $href = $link->attr('href');
+        $linkCount = collect($internalLinks)
+            ->map(function ($element) {
+                $href = $element->attr('href');
 
-            if (Str::startsWith($href, '/')) {
-                $href = Crawler::DOMAIN_PREFIX . $href;
-            }
+                if (Str::startsWith($href, '/')) {
+                    $href = Crawler::DOMAIN_PREFIX . $href;
+                }
 
-            $result->addInternalLink($href);
-        }
+                return $href;
+            })
+            ->unique()
+            ->count();
+
+        $result->internalLinkCount = $linkCount;
 
         return $next($result);
     }
