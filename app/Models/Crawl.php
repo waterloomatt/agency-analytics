dc69d365ad02fa788494395c35e2ddcd6bba0a67
Matt Skelton <waterloomatt@gmail.com>
2023-03-17 12:11:58 -0400

    Add recent scope

--- app/Models/Crawl.php
+++ app/Models/Crawl.php
@@ -6,12 +6,14 @@ use App\Enums\CrawlStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Crawl extends Model
 {
     use HasFactory;
 
-    protected $guarded  = [];
+    protected $guarded = [];
+
     protected $casts = [
         'status' => CrawlStatus::class,
     ];
@@ -19,5 +21,10 @@ class Crawl extends Model
     public function details(): HasMany
     {
         return $this->hasMany(CrawlDetail::class);
+    }
+
+    public function scopeRecent(Builder $query): Builder
+    {
+        return $query->latest()->limit(5);
     }
 }
