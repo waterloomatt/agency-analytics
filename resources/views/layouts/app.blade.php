cfed7db157c369afa6ab442a8e4de53c6343a590
Matt Skelton <waterloomatt@gmail.com>
2023-03-17 21:44:25 -0400

    Fix formatting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,18 @@
 <html lang="">
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <title>AgencyAnalytics - @yield('title')</title>
 </head>
+
 <body>
 
-<div class="container mx-auto">
-    @yield('content')
-</div>
+    <div class="container mx-auto">
+        @yield('content')
+    </div>
 </body>
+
 </html>
--- resources/views/partials/crawl_details_modal.blade.php
+++ resources/views/partials/crawl_details_modal.blade.php
@@ -1,16 +1,17 @@
 <div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center"
-     id="{{ $modal_id }}">
+    id="{{ $modal_id }}">
     <div class="relative w-auto my-6 mx-auto max-w-3xl">
-        <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+        <div
+            class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
             <div class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                 <h3 class="text-3xl font-semibold">
                     HTTP Status Codes
                 </h3>
                 <button
-                        class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                        onclick="toggleModal('{{ $modal_id }}')">
+                    class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onclick="toggleModal('{{ $modal_id }}')">
                     <span
-                            class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                        class="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                         ×
                     </span>
                 </button>
@@ -23,30 +24,30 @@
                             <div class="border-t border-gray-200">
                                 <table class="min-w-full leading-normal">
                                     <thead>
-                                    <tr>
-                                        <th
+                                        <tr>
+                                            <th
                                                 class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                            URL
-                                        </th>
-                                        <th
+                                                URL
+                                            </th>
+                                            <th
                                                 class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                            HTTP Status
-                                        </th>
-                                    </tr>
+                                                HTTP Status
+                                            </th>
+                                        </tr>
                                     </thead>
                                     <tbody>
 
-                                    @foreach ($crawl->details as $crawlPage)
-                                        <tr>
-                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <p class="text-gray-900 whitespace-no-wrap">{{ $crawlPage->url }}
-                                                </p>
-                                            </td>
-                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                <p class="text-gray-900 whitespace-no-wrap">
-                                                    {{ $crawlPage->http_status }}</p>
-                                            </td>
-                                    @endforeach
+                                        @foreach ($crawl->details as $crawlPage)
+                                            <tr>
+                                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                    <p class="text-gray-900 whitespace-no-wrap">{{ $crawlPage->url }}
+                                                    </p>
+                                                </td>
+                                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                    <p class="text-gray-900 whitespace-no-wrap">
+                                                        {{ $crawlPage->http_status }}</p>
+                                                </td>
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
@@ -58,14 +59,12 @@
 
             <div class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                 <button
-                        class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                        type="button"
-                        onclick="toggleModal('{{ $modal_id }}')">
+                    class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button" onclick="toggleModal('{{ $modal_id }}')">
                     Close
                 </button>
             </div>
         </div>
     </div>
 </div>
-<div class="hidden opacity-25 fixed inset-0 z-40 bg-black"
-     id="{{ $modal_id }}-backdrop"></div>
+<div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="{{ $modal_id }}-backdrop"></div>
